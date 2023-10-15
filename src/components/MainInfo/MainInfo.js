9ad8c6381b09adb0eb2e347c345cacf7d18d7af3
MikeTR <miguel_tovarrodriguez@hotmail.com>
2023-10-15 12:30:05 -0600

    created app and started to set a structure

---
+++ src/components/MainInfo/MainInfo.js
@@ -0,0 +1,15 @@
+import './MainInfo.css';
+
+function MainInfo(){
+
+    return(
+        <div className="container">
+            <div className="main-info">
+                <h2>Tu Información</h2>
+                {/* Agrega tu información aquí */}
+            </div>
+        </div>
+    )
+};
+
+export default MainInfo;
